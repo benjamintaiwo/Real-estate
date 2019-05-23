a85d11a54ff34f6d2f8dda1d1aa096c1f1972629
benjamintaiwo <abegundetaiwo@gmail.com>
2019-05-24 00:23:45 +0100

    ADD: send mail function

--- contatcts/views.py
+++ contatcts/views.py
@@ -25,6 +25,10 @@ def contact(request):
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id )
         contact.save()
 
+        send_mail(
+            'Property Listing for Enquiry',
+            'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+            'taiwoabegunde@live.com', realtor_email, fail_silently=False)
         
 
          messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
