5394b097cffdb699f7918473784f00d91e02bb5b
benjamintaiwo <abegundetaiwo@gmail.com>
2019-05-25 12:57:36 +0100

    Fix: heroku

--- Real_Estate/settings.py
+++ Real_Estate/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -148,3 +148,6 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:8000',
     '127.0.0.1:8000'
 )
+
+
+django_heroku.settings(locals())
